4811b2c61b4a544c786649f1698329b3d65659d3
yangping8808 <yangping8808@163.com>
2022-08-26 21:56:59 +0800

    增加allure

--- src/main/java/Base/Common.java
+++ src/main/java/Base/Common.java
@@ -1,7 +1,11 @@
 package Base;
 
 import org.apache.commons.lang3.RandomStringUtils;
+import org.jetbrains.annotations.NotNull;
 
+import java.io.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Random;
 
 public class Common {
@@ -10,7 +14,9 @@ public class Common {
         System.out.println("=========  main start ===============");
         randomInt();
         randomIntStr(10);
-        System.out.println("========= main end ===============");
+        currentDate();
+        createDirDate("\\allure-results\\");
+        exeCmd("ipconfig");
     }
 
     public static void randomInt() {
@@ -24,12 +30,44 @@ public class Common {
         System.out.println(str);
     }
 
-
-    public static void createDir() {
-
+    public static @NotNull String currentDate(){
+        Date date = new Date();    // 获取当前时间
+//        LocalDateTime dateTime = LocalDateTime.now(); // get the current date and time
+        SimpleDateFormat formatter = new SimpleDateFormat("[yyyy-MM-dd-HH-mm-ss]");
+        return formatter.format(date);
     }
 
+    public static String createDirDate(String mypath) {
+        String relatePath = System.getProperty("user.dir");   // D:\IDEA\workspace\Epro   项目根目录
+        String directoryPath = relatePath + mypath + currentDate();
+        File file = new File(directoryPath);
+        if (!file.exists()) {    //文件夹不存在
+            if (file.mkdirs()) {
+                System.out.println("创建文件夹 " + directoryPath + " 成功");
+            } else {
+                System.out.println("创建文件夹 " + directoryPath + " 失败");
+            }
+        }
+        return directoryPath;
+    }
 
+    // Java调用 dos窗口命令
+    public static void exeCmd(String cmd) {
+        try {
+            Process process = Runtime.getRuntime().exec(cmd);
+            InputStream is = process.getInputStream();
+            InputStreamReader isr = new InputStreamReader(is,"GBK");
+            BufferedReader br = new BufferedReader(isr);
+            String content = br.readLine();
+            while (content != null) {
+                System.out.println(content);
+                content = br.readLine();
+            }
+        } catch (
+                IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
 
 
--- src/test/java/Base/TestngRun.java
+++ src/test/java/Base/TestngRun.java
@@ -3,14 +3,26 @@ package Base;
 import org.testng.TestNG;
 import org.testng.collections.Lists;
 
+import java.io.IOException;
 import java.util.List;
+import Base.Common.*;
 
 public class TestngRun {
+
     public static void main(String[] args) {
         TestNG testng = new TestNG();
         List suites = Lists.newArrayList();
         suites.add("D:\\IDEA\\workspace\\Epro\\testng.xml");//path to xml..
         testng.setTestSuites(suites);
         testng.run();
+//        //生成allure报告
+//        String allureDir = Common.createDirDate("\\allure-results\\");
+//        String cmdAllure = "allure generate " + allureDir + " -o " + allureDir + "\\html" + " --clean";
+//        System.out.println(cmdAllure);
+//        Common.exeCmd(cmdAllure);
     }
+
+
+
+
 }
